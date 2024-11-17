0bf3017384f02395a1a854a662a1e52743998a90
davidcrm <davidcm220301@gmail.com>
2024-11-17 13:25:36 +0000

    finalfinal

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Skills from './sections/Skills/Skills';
-import Projects from './sections/projects/Projects';
+import Projects from './sections/Projects/Projects';
 function App() {
   
   return(
